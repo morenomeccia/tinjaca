f160c5738658c1d19ff6e46090b9e62b05c3a40b
Francisco Palm <francisco.palm@gmail.com>
2015-05-15 14:15:11 -0430

    Add diagram codes 21 and 22.

---
+++ doc/informe1/fomdes_proc21.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+"""
+BPMN diagram for FOMDES process 1
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from bpmn_pgv import *
+import pygraphviz as pgv
+
+__author__ = 'mapologo'
+
+PROCESS_LABEL = "Recepción y Evaluación de Solicitudes 1"
+# A graph for FOMDES processes
+F = pgv.AGraph(strict=False, directed=True)
+
+F.graph_attr.update(label=TITLE_TEMPLATE.format(PROCESS_LABEL), rankdir="TB", splines="ortho", labelloc="b",
+                    size="18, 8", forcelabels="true", ranksep="0.25", fontname="Liberation Sans Narrow Condensed")
+F.node_attr.update(fontname="Liberation Sans Narrow Condensed")
+F.edge_attr.update(fontname="Liberation Sans Narrow Condensed", fontsize="10")
+
+u_cluster = {"u4": ("Recibir Requisitos", "start"),
+             "u5": ("Reunir Requisitos", "human"),
+             "u6": ("Consignar los Requisitos", "message"),
+             "u7": ("Fin Requisitos", "end")}
+
+u_edges = {"u4": {"u5": {}},
+           "u5": {"u6": {}},
+           "u6": {"u7": {}},
+           "u7": {}}
+
+U = add_cluster(F, "u", "Usuario / Beneficiario", u_cluster, u_edges)
+
+ic_cluster = {"ic4": ("Recibir Requisitos", "start"),
+              "ic5": ("Crear Expediente", "human"),
+              "ic6": ("Entrevista con el usuario", "human"),
+              "ic7": ("", "end")}
+
+ic_edges = {"ic4": {"ic5": {}},
+            "ic5": {"ic6": {}},
+            "ic6": {"ic7": {"style":"invis"}}}
+
+IC = add_cluster(F, "ic", "Información de Crédito", ic_cluster, ic_edges)
+
+aj_cluster = {"aj0": ("Verificar validez legal de los requisitos", "human"),
+              "aj1": ("Elaborar Informe de Control Previo", "human"),
+              "aj2": ("¿La garantía cumple?", "exclusive"),
+              "aj3": ("Enviar Expediente a Análisis Económico", "message")}
+
+aj_edges = {"aj0": {"aj1": {}},
+            "aj1": {"aj2": {}},
+            "aj2": {"aj3": {"xlabel": "Si"}}}
+
+AJ = add_cluster(F, "aj", "Análisis Jurídico", aj_cluster, aj_edges)
+
+
+
+#F.add_node("Webadmin", image="static/database.png", shape="plaintext", label="", xlabel="Webadmin")
+
+global_edges = {"u6":  {"ic4": {"style": "dashed"}},
+                "ic6": {"aj0": {"style": "dashed"}},
+                "aj3": {"ic7": {"style": "dashed"}, "Análisis Económico": {"style": "dashed"}}}
+
+add_edges(F, global_edges)
+
+F.draw("proc21.png", prog='dot')
+F.write("proc21.dot")
---
+++ doc/informe1/fomdes_proc22.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+"""
+BPMN diagram for FOMDES process 1
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from bpmn_pgv import *
+import pygraphviz as pgv
+
+__author__ = 'mapologo'
+
+PROCESS_LABEL = "Recepción y Evaluación de Solicitudes 2"
+# A graph for FOMDES processes
+F = pgv.AGraph(strict=False, directed=True)
+
+F.graph_attr.update(label=TITLE_TEMPLATE.format(PROCESS_LABEL), rankdir="TB", splines="ortho", labelloc="b",
+                    size="8, 8", forcelabels="true", ranksep="0.25", fontname="Liberation Sans Narrow Condensed")
+F.node_attr.update(fontname="Liberation Sans Narrow Condensed")
+F.edge_attr.update(fontname="Liberation Sans Narrow Condensed", fontsize="10")
+
+ae_cluster = {"ae0": ("Recibir Expediente", "start"),
+              "ae1": ("Planificar las rutas", "human"),
+              "ae2": ("Realizar la Inspección", "human"),
+              "ae3": ("¿Requiere Garantía?", "exclusive"),
+              "ae4": ("Realizar avalúo de la garantía", "human"),
+              "ae5": ("", "exclusive"),
+              "ae6": ("Realizar Informe Técnico", "human"),
+              "ae7": ("Enviar Expediente al Gerente de Crédito", "message"),
+              "ae8": ("", "end")}
+
+ae_edges = {"ae0": {"ae1": {}},
+            "ae1": {"ae2": {}},
+            "ae2": {"ae3": {}},
+            "ae3": {"ae4": {"xlabel": "Si"}, "ae5": {"xlabel": "No"}},
+            "ae4": {"ae5": {}},
+            "ae5": {"ae6": {}},
+            "ae6": {"ae7": {}},
+            "ae7": {"ae8": {}}}
+
+AE = add_cluster(F, "ae", "Análisis Económico", ae_cluster, ae_edges)
+
+gc_cluster = {"gc0": ("Recibir Expediente", "start"),
+              "gc1": ("Revisión del Expediente", "human"),
+              "gc2": ("Dictamen del Informe Técnico", "complex"),
+              "gc3": ("Enviar Expediente a Secretaría Ejecutiva", "message"),
+              "gc4": ("", "end")}
+
+gc_edges = {"gc0": {"gc1": {}},
+            "gc1": {"gc2": {}},
+            "gc2": {"gc3": {"xlabel": "Aprobado"}},
+            "gc3": {"gc4": {}}}
+
+GC = add_cluster(F, "gc", "Gerencia de Crédito", gc_cluster, gc_edges)
+
+
+#F.add_node("Webadmin", image="static/database.png", shape="plaintext", label="", xlabel="Webadmin")
+
+global_edges = {"Ánálisis Jurídico": {"ae0": {"style": "dashed"}},
+                "ae7": {"gc0": {"style": "dashed"}},
+                "gc0": {"ae0": {"style": "invis"}}}
+
+add_edges(F, global_edges)
+
+F.draw("proc22.png", prog='dot')
+F.write("proc22.dot")
