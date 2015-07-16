13707583813ba26b45b94607d4a312763a82ad59
Sirius Fuenmayor <fuenmayor.sirius@gmail.com>
2015-07-16 15:14:31 -0430

    Modificaciones al modelo de informe tecnico

--- addons/solicitudes/models/model_informe_tecnico.py
+++ addons/solicitudes/models/model_informe_tecnico.py
@@ -4,14 +4,14 @@ from openerp import models, fields, api
 
 
 class InformeTecnico(models.Model):
-    _name = 'solicitudes.informe_tecnico'
+    _name = 'openacademy.informe_tecnico'
 
     nro_expediente = fields.Char(string='Numero de expediente', required=True)  # !!!
     fecha_elaboracion = fields.Date(string='Fecha de elaboracion', required=True)
-    tipo_empresa = fields.Char(string='Tipo de empresa', required=True)  # !!!
-    Saldo_Balance_Personal = fields.Float(string='Balance personal', required=True)
-    Organizacion_Juridica = fields.Char(string='Organizacion juridica', required=True)
-    Recomendaciones = fields.Char(string='Recomendaciones', required=True)
+    tipo_empresa = fields.Char(string='Tipo de empresa')  # !!!
+    Saldo_Balance_Personal = fields.Float(string='Balance personal')
+    Organizacion_Juridica = fields.Char(string='Organizacion juridica')
+    Recomendaciones = fields.Char(string='Recomendaciones')
     firma = fields.Binary(string='Firma', required=True)
     informe_fotografico_inspeccion = fields.Binary(string='Fotografia de la inspeccion', required=True)
     codigo_credito = fields.Integer(string='Codigo del credito', required=True)
