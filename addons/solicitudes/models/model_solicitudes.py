4e29ca9bfeb0e0a9dd6313c13cdd284261ee3dfc
Sirius Fuenmayor <fuenmayor.sirius@gmail.com>
2015-07-20 22:42:05 -0430

    Se agrego informe tecnico

--- addons/solicitudes/models/model_solicitudes.py
+++ addons/solicitudes/models/model_solicitudes.py
@@ -6,4 +6,4 @@ class Solicitudes(models.Model):
     _name = 'solicitudes.solicitudes'
 
     nro_expediente = fields.Char(string='Numero de expediente', required=True)
-    #informe_tecnico_id = fields.One2many('solicitudes.informe_tecnico', string="Informe tecnico") 
+    informe_tecnico_id = fields.One2many('solicitudes.informe_tecnico', 'solicitudes_id', string="Informe tecnico") 
