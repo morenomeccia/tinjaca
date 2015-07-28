78963217b145b865e07e502f119afd6035432f8a
Francisco Palm <francisco.palm@gmail.com>
2015-07-27 22:45:31 -0430

    Delete commented code from Solicitudes model, rename nro_expediente field as name

--- addons/solicitudes/models/model_solicitudes.py
+++ addons/solicitudes/models/model_solicitudes.py
@@ -5,5 +5,14 @@ from openerp import models, fields, api
 class Solicitudes(models.Model):
     _name = 'solicitudes.solicitudes'
 
-    nro_expediente = fields.Char(string='Numero de expediente', required=True)
-    informe_tecnico_id = fields.One2many('solicitudes.informe_tecnico', 'solicitudes_id', string="Informe tecnico") 
+    name = fields.Char(string='Numero de expediente', required=True)
+    informe_tecnico_id = fields.One2many('solicitudes.informe_tecnico', 'solicitudes_id', string="Informe tecnico")
+
+    # display_name = fields.Char(
+    #     string='Número de expediente', compute='_compute_display_name',
+    # )
+    #
+    # @api.one
+    # @api.depends('nro_expediente')
+    # def _compute_display_name(self):
+    #     self.display_name = self.nro_expediente
