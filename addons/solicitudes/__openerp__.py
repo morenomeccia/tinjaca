53dd4c5978236c5d9346ff6f99a561ccb7a67c3f
Francisco Palm <francisco.palm@gmail.com>
2015-08-08 21:53:45 -0430

    creacion de grupos y permisos para las solicitudes

--- addons/solicitudes/__openerp__.py
+++ addons/solicitudes/__openerp__.py
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'templates.xml',
         'views/solicitudes.xml',
         'views/solicitudes_workflow.xml',
