087350e4eaf054ec2863884ae9223cabce04fd36
alemary <alemaryrujano@gmail.com>
2015-08-21 21:30:50 -0430

    Se añadieron campos relacionales del módulo propuesta

--- addons/propuestas/models/model_propuestas.py
+++ addons/propuestas/models/model_propuestas.py
@@ -8,3 +8,8 @@ class Propuestas(models.Model):
      solicitante_id = fields.Many2one('propuestas.solicitantes', string="Solicitante")
      unidad_prod_id = fields.Many2one('propuestas.unidades_productivas', string="Unidad Productiva")
      actividad_prod_id = fields.Many2one('propuestas.actividades_productivas', string="Actividad Productiva")
+     avalista_id = fields.Many2one('propuestas.avalistas', string="Avalista")
+     garantia_id = fields.Many2one('propuestas.garantias', string="Garantia")
+     referencias_solicitante_id = fields.Many2one('propuestas.referencias_solicitantes', string="Referencias del Solicitante")
+     referencias_avalista_id = fields.Many2one('propuestas.referencias_avalistas', string='Referencias del Avalista')
+     plan_inversion_id = fields.Many2one('propuestas.planes_inversion', string='Plan de Inversión')
