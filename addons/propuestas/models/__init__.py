e27f706c31ba172c4f10fcfe00596dcf6522c364
Francisco Palm <francisco.palm@gmail.com>
2015-08-23 15:02:43 -0430

    add __init__.py to propuestas/models

---
+++ addons/propuestas/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import model_actividades_productivas
+import model_avalistas
+import model_garantias
+import model_planes_inversion
+import model_propuestas
+import model_referencias_solicitante
+import model_solicitantes
+import model_unidades_productivas
