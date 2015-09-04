2cb3db85f22c028ad010564220e08f167240a1c6
Francisco Palm <francisco.palm@gmail.com>
2015-09-04 16:02:31 -0430

    Comentar campos que deben estar en el módulo Solicitudes

--- addons/propuestas/models/model_propuestas.py
+++ addons/propuestas/models/model_propuestas.py
@@ -6,11 +6,11 @@ from openerp import models, fields, api
 class Propuestas(models.Model):
      _name = 'propuestas.propuestas'
 
-     solicitante_id = fields.Many2one('propuestas.solicitantes', string="Solicitante")
-     unidad_prod_id = fields.Many2one('propuestas.unidades_productivas', string="Unidad Productiva")
-     actividad_prod_id = fields.Many2one('propuestas.actividades_productivas', string="Actividad Productiva")
-     avalista_id = fields.Many2one('propuestas.avalistas', string="Avalista")
-     garantia_id = fields.Many2one('propuestas.garantias', string="Garantia")
-     referencias_solicitante_id = fields.Many2one('propuestas.referencias_solicitantes', string="Referencias del Solicitante")
-     referencias_avalista_id = fields.Many2one('propuestas.referencias_avalistas', string='Referencias del Avalista')
-     plan_inversion_id = fields.Many2one('propuestas.planes_inversion', string='Plan de Inversión')
+     solicitante_id = fields.Many2one('propuestas.solicitantes', 'propuesta_id', string="Solicitante")
+     unidad_prod_id = fields.Many2one('propuestas.unidades_productivas', 'propuesta_id', string="Unidad Productiva")
+     actividad_prod_id = fields.Many2one('propuestas.actividades_productivas', 'propuesta_id', string="Actividad Productiva")
+     # avalista_id = fields.Many2one('propuestas.avalistas', 'propuesta_id', string="Avalista")
+     # garantia_id = fields.Many2one('propuestas.garantias', 'propuesta_id', string="Garantia")
+     referencias_solicitante_id = fields.Many2one('propuestas.referencias_solicitantes', 'propuesta_id', string="Referencias del Solicitante")
+     # referencias_avalista_id = fields.Many2one('propuestas.referencias_avalistas', 'propuesta_id', string='Referencias del Avalista')
+     plan_inversion_id = fields.Many2one('propuestas.planes_inversion', 'propuesta_id', string='Plan de Inversión')
