f0b0e2fc3a2f5e3ec7511c2620b938176f70f52e
Francisco Palm <francisco.palm@gmail.com>
2015-09-10 17:22:15 -0430

    Change propuestas model to inherit from res.users

--- addons/propuestas/models/model_propuestas.py
+++ addons/propuestas/models/model_propuestas.py
@@ -6,11 +6,11 @@ from openerp import models, fields, api
 class Propuestas(models.Model):
      _name = 'propuestas.propuestas'
 
-     solicitante_id = fields.Many2one('propuestas.solicitantes', 'propuesta_id', string="Solicitante")
-     unidad_prod_id = fields.Many2one('propuestas.unidades_productivas', 'propuesta_id', string="Unidad Productiva")
-     actividad_prod_id = fields.Many2one('propuestas.actividades_productivas', 'propuesta_id', string="Actividad Productiva")
+     solicitante_id = fields.Many2one(comodel_name='propuestas.solicitantes', string="Solicitante")
+     unidad_prod_id = fields.Many2one(comodel_name='propuestas.unidades_productivas', string="Unidad Productiva")
+     actividad_prod_id = fields.Many2one(comodel_name='propuestas.actividades_productivas', string="Actividad Productiva")
      # avalista_id = fields.Many2one('propuestas.avalistas', 'propuesta_id', string="Avalista")
      # garantia_id = fields.Many2one('propuestas.garantias', 'propuesta_id', string="Garantia")
-     referencias_solicitante_id = fields.Many2one('propuestas.referencias_solicitantes', 'propuesta_id', string="Referencias del Solicitante")
+     referencias_solicitante_id = fields.Many2one(comodel_name='propuestas.referencias_solicitantes', string="Referencias del Solicitante")
      # referencias_avalista_id = fields.Many2one('propuestas.referencias_avalistas', 'propuesta_id', string='Referencias del Avalista')
-     plan_inversion_id = fields.Many2one('propuestas.planes_inversion', 'propuesta_id', string='Plan de Inversión')
+     plan_inversion_id = fields.Many2one(comodel_name='propuestas.planes_inversion', string='Plan de Inversión')
