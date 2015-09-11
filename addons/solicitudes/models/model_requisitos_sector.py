8dd65cc9d7eff665fd12553b3f9a783e2d3c07a7
Sirius Fuenmayor <fuenmayor.sirius@gmail.com>
2015-09-10 22:04:34 -0430

    Se revierte el filtrado de requisitos por sector y que no funciona correctamente

--- addons/solicitudes/models/model_requisitos_sector.py
+++ addons/solicitudes/models/model_requisitos_sector.py
@@ -6,7 +6,7 @@ class RequisitosSector(models.Model):
     _name = 'solicitudes.requisitos_sector'
 
     solicitudes_id = fields.Many2one('solicitudes.solicitudes', string="Número de expediente")
-    sector2 = fields.Selection(string='Sector', related='solicitudes_id.sector', store=True)
+    sector2 = fields.Selection(string='Sector', related='solicitudes_id.sector')
     documentos_sector_id = fields.Many2one('solicitudes.documentos_sector', string="Tipo de Documento")
     documento = fields.Binary(string='Documento')
     observaciones = fields.Char(string='Observaciones')
