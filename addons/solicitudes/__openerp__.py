9dca61101158ccec47a5497695a74b6af6720859
acth <adrianac.torresh@gmail.com>
2015-09-25 21:55:44 -0430

    Se cambian datos del modulo para su instalacion e informacion a mostrar

--- addons/solicitudes/__openerp__.py
+++ addons/solicitudes/__openerp__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "solicitudes",
+    'name': "Tinjaca-Solicitudes",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        Modulo para la gestion de solicitudes de credito del FOMDES
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        Tinjaca - Modulo Solicitudes (FOMDES)
+    """,
+
+    'author': "Cooperativa Saní Tecnologías Comunes",
+    'website': "http://sani.org.ve",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -36,4 +36,7 @@
     'demo': [
         'demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
