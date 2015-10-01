1df589d1535f2bfcf3f8baea5c2abc82008547c8
Sirius Fuenmayor <fuenmayor.sirius@gmail.com>
2015-09-30 21:36:32 -0430

    Se corrigio la vista de solicitudes

--- addons/solicitudes/models/requisitos_generales.py
+++ addons/solicitudes/models/requisitos_generales.py
@@ -7,7 +7,7 @@ class RequisitosGenerales(models.Model):
     _name = 'solicitudes.requisitos_generales'
 
     solicitudes_id = fields.Many2one('solicitudes.solicitudes', string="Número de expediente")
-    tipo_documento = fields.Many2one('solicitudes.documentos_generales', string="Tipo de Documento")
+    documentos_generales_id = fields.Many2one('solicitudes.documentos_generales', string="Tipo de Documento")
     documento = fields.Binary(string='Documento')
     observaciones = fields.Char(string='Observaciones')
     valido = fields.Boolean(string='Valido')
