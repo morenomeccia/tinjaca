2830728551c9225ab891022f183d9a3e2298cc48
Sirius Fuenmayor <fuenmayor.sirius@gmail.com>
2015-11-21 19:38:30 -0430

    Se extendio el workflow de solicitudes para incluir a las estaciones de secretaria ejecutiva

--- addons/solicitudes/models/solicitudes.py
+++ addons/solicitudes/models/solicitudes.py
@@ -22,11 +22,18 @@ class Solicitudes(models.Model):
     state = fields.Selection(string='Estacion', selection=[('estacion_1_analisis_credito', 'Informacion de credito'),
                                                            ('estacion_2_analisis_juridico', 'Analisis juridico'),
                                                            ('estacion_3_analisis_economico', 'Analisis economico'),
-                                                           ('estacion_4_gerencia_credito', 'Gerencia de credito')], default='estacion_1_analisis_credito')
-    estatus_consejo_directivo = fields.Selection(string='Estatus', selection=[('estatus_discusion', 'Discusion'),
-                                                                              ('estatus_rechazado', 'Rechazado'),
-                                                                              ('estatus_aprobado', 'Aprobado'),
-                                                                              ('estatus_diferido', 'Diferido')], default='estatus_discusion')
+                                                           ('estacion_4_gerencia_credito', 'Gerencia de credito'),
+                                                           ('estacion_5_verificar_secretaria', 'Secretaria ejecutiva (por verificar)'),
+                                                           ('estacion_6_aprobar_secretaria', 'Secretaria ejecutiva (por aprobar)'), 
+                                                           ('estacion_7_crear_documentos_secretaria', 'Secretaria ejecutiva (crear documentos)'),
+                                                           ('estacion_8_liquidacion', 'Liquidacion')], 
+							 default='estacion_1_analisis_credito')
+    
+
+	#estatus_consejo_directivo = fields.Selection(string='Estatus', selection=[('estatus_discusion', 'Discusion'),
+    #                                                                          ('estatus_rechazado', 'Rechazado'),
+    #                                                                          ('estatus_aprobado', 'Aprobado'),
+    #                                                                          ('estatus_diferido', 'Diferido')], default='estatus_discusion')
 
     requisitos_generales_id = fields.One2many('solicitudes.requisitos_generales', 'solicitudes_id', string="Requisitos generales")
     requisitos_sector_id = fields.One2many('solicitudes.requisitos_sector', 'solicitudes_id', string="Requisitos sector")
@@ -53,6 +60,36 @@ class Solicitudes(models.Model):
     @api.one
     def action_enviar_gerencia_credito(self):
         self.state = 'estacion_4_gerencia_credito'
+
+	# Envia a la estacion "Secretaria ejecutiva (por verificar)", boton "Enviar a secretaria ejecutiva"
+    @api.one
+    def action_enviar_secretaria_verificar(self):
+        self.state = 'estacion_5_verificar_secretaria'
+
+	# Envia a la estacion "Secretaria ejecutiva (por aprobar)", boton "verificado"
+    @api.one
+    def action_enviar_secretaria_aprobar(self):
+        self.state = 'estacion_6_aprobar_secretaria'
+
+	# Envia a la estacion "Secretaria ejecutiva (crear documentos)", boton "aprobar"
+    @api.one
+    def action_enviar_secretaria_documentar(self):
+        self.state = 'estacion_7_crear_documentos_secretaria'
+
+	# Envia a la estacion "Gerencia de credito", boton "Rechazar"
+    @api.one
+    def action_secretaria_rechazar(self):
+        self.state = 'estacion_4_gerencia_credito'
+
+	# Envia a la estacion "Gerencia de credito", boton "Diferir"
+    @api.one
+    def action_secretaria_diferir(self):
+        self.state = 'estacion_4_gerencia_credito'
+
+	# Envia a la estacion "Liquidacion", boton "Enviar a liquidacion"
+    @api.one
+    def action_enviar_liquidacion(self):
+        self.state = 'estacion_8_liquidacion'
 
     # display_name = fields.Char(
     #     string='Número de expediente', compute='_compute_display_name',
