222d40137b7c3763300c6357db47e23f7d93e84a
Sirius Fuenmayor <fuenmayor.sirius@gmail.com>
2015-11-23 17:04:08 -0430

    Se migran los campos relacionados a los montos de cada sector al modelo Sector. Se elimina el modelo de Montos

--- addons/politicas/models/__init__.py
+++ addons/politicas/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import sectores
-import montos
 import documentos_sector
 import documentos_garantia
 import documentos_generales
--- addons/politicas/models/sectores.py
+++ addons/politicas/models/sectores.py
@@ -11,3 +11,12 @@ class Sectores(models.Model):
     descripcion = fields.Char(string='Descripcion')
     habilitado = fields.Boolean(string='Habilitar')
 
+    tasa_interes_promocionales = fields.Float(string='Tasa de interes (empresas promocionales)')
+    tasa_interes_establecidas = fields.Float(string='Tasa de interes (empresas establecidas)')
+    tasa_mora = fields.Float(string='Tasa de mora')
+    comision_flat = fields.Float(string='Comision FLAT')
+    aporte_social = fields.Float(string='Aporte social')
+    plazo_maximo = fields.Integer(string='Plazo maximo de pago')
+    monto_maximo = fields.Float(string='Monto maximo')
+    monto_minimo = fields.Float(string='Monto minimo')
+
