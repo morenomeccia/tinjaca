56ed1c46b0ca56d696ffcaab42da5ca6757d0bdc
Francisco Palm <francisco.palm@gmail.com>
2015-12-04 23:46:53 -0430

    Añadiendo un acento

--- addons/propuestas/models/propuestas.py
+++ addons/propuestas/models/propuestas.py
@@ -10,7 +10,7 @@ class Propuestas(models.Model):
      unidad_prod_id = fields.Many2one(comodel_name='propuestas.unidades_productivas', string="Unidad Productiva")
      actividad_prod_id = fields.Many2one(comodel_name='propuestas.actividades_productivas', string="Actividad Productiva")
      avalista_id = fields.Many2one(comodel_name='propuestas.avalistas', string="Avalista")
-     garantia_id = fields.Many2one(comodel_name='propuestas.garantias', string="Garantia")
+     garantia_id = fields.Many2one(comodel_name='propuestas.garantias', string="Garantía")
      referencias_sol_id = fields.Many2one(comodel_name='propuestas.referencias_solicitantes', string="Referencias del Solicitante")
      # referencias_aval_id = fields.Many2one(comodel_name='propuestas.referencias_avalistas', string='Referencias del Avalista')
      plan_inversion_id = fields.Many2one(comodel_name='propuestas.planes_inversion', string='Plan de Inversión')
