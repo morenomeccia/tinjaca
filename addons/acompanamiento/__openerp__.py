527d478daa7170bf5fb88acbd47c8fa29bd66ada
acth <adrianac.torresh@gmail.com>
2015-12-26 18:02:51 -0430

    Se agregan las vistas dentro del manifiesto de Acompanamiento

--- addons/acompanamiento/__openerp__.py
+++ addons/acompanamiento/__openerp__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        # 'templates.xml',
+         'views/acompanamiento_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
