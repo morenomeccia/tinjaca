c273026e2982cd6064eebe37a5ecaf48e13e41e6
acth <adrianac.torresh@gmail.com>
2015-12-26 20:05:39 -0430

    Se agregan los campos solicitudes_id y nro_expediente_solicitudes al modelo creditos del modulo Administracion

--- addons/administracion/models/creditos.py
+++ addons/administracion/models/creditos.py
@@ -3,7 +3,12 @@
 from openerp import models, fields, api
 
 class Creditos(models.Model):
-    _name = 'solicitudes.creditos'
+    _name = 'administracion.creditos'
+    _rec_name = 'nro_expediete_solicitudes'
 
     fecha_liquidacion = fields.Date(string='Fecha de liquidacion', required=True)
     fecha_ultima = fields.Date(string='Fecha ultima', required=True) #calculado
+
+    solicitudes_id = fields.Many2one('solicitudes.solicitudes', string = "Numero de expediente")
+
+    nro_expediente_solicitudes = fields.Char(string='Numero de Expediente', related='solicitudes_id.nro_expediente', readonly=True)
