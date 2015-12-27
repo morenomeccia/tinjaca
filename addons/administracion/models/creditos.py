db43af1626fd75da00ad6ccace81a9034b10e80f
acth <adrianac.torresh@gmail.com>
2015-12-26 20:47:42 -0430

    Correccion del nombre del campo fecha_ultima en creditos.py del modulo Administracion

--- addons/administracion/models/creditos.py
+++ addons/administracion/models/creditos.py
@@ -4,11 +4,11 @@ from openerp import models, fields, api
 
 class Creditos(models.Model):
     _name = 'administracion.creditos'
-    _rec_name = 'nro_expediete_solicitudes'
+    _rec_name = 'nro_expediente_solicitudes'
 
     fecha_liquidacion = fields.Date(string='Fecha de liquidacion', required=True)
-    fecha_ultima = fields.Date(string='Fecha ultima', required=True) #calculado
+    fecha_ultima = fields.Date(string='Fecha ultima de pago', required=True) #calculado
 
-    solicitudes_id = fields.Many2one('solicitudes.solicitudes', string = "Numero de expediente")
+    solicitudes_id = fields.Many2one('solicitudes.solicitudes', string = "Numero de Expediente")
 
     nro_expediente_solicitudes = fields.Char(string='Numero de Expediente', related='solicitudes_id.nro_expediente', readonly=True)
