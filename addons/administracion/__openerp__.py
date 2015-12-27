5700268d743212c6ff2de716cc90b6a9b313f816
acth <adrianac.torresh@gmail.com>
2015-12-26 20:25:35 -0430

    Creacion del manifiesto del modulo Administracion

--- addons/administracion/__openerp__.py
+++ addons/administracion/__openerp__.py
@@ -1,34 +1,39 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "administracion",
+    'name': "Tinjaca - Administracion",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        Gestion administrativa de los creditos aprobados por el FOMDES
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        Tinjaca - Modulo Administracion (FOMDES)
+    """,
+
+    'author': "Cooperativa Saní Tecnologías Comunes",
+    'website': "http://sani.org.ve",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tinjaca',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','fomdes','solicitudes'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'templates.xml',
+        #'templates.xml',
+        'views/administracion.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo.xml',
     ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
