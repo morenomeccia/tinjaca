415afc41811bf107aeace9bfd3d0e85de851f65c
Jorge Moreno <morenomeccia@yahoo.com>
2016-01-03 17:22:00 -0430

    Correcciones en los modelos y vistas de cuentas por cobrar y partidas

--- addons/administracion/models/cuentas_cobrar.py
+++ addons/administracion/models/cuentas_cobrar.py
@@ -2,9 +2,9 @@ from openerp import models, fields, api
 
 class CuentasCobrar(models.Model):
     _name = 'administracion.cuentas_cobrar'
-    _rec_name = 'descripcion'
+    _rec_name = 'codigo'
 
-    codigo = fields.Char(string= "Codigo de cuenta")
+    codigo = fields.Char(string= "Codigo de cuenta", required=True)
     descripcion = fields.Char(string="Descripcion")
     solicitudes_id = fields.Many2one('solicitudes.solicitudes', string='Numero de expediente', required=True)
-    partidas_id = fields.Many2one('administracion.partidas', string ='Partida presupuestaria')
+    partidas_id = fields.Many2one('administracion.partidas', string ='Partida presupuestaria', required=True)
--- addons/administracion/models/partidas.py
+++ addons/administracion/models/partidas.py
@@ -2,8 +2,9 @@ from openerp import models, fields, api
 
 class Partidas(models.Model):
     _name = 'administracion.partidas'
-    _rec_name = 'descripcion'
+    _rec_name = 'codigo'
 
-    codigo = fields.Char(string= "Codigo de cuenta")
+    codigo = fields.Char(string= "Codigo de partida", required=True)
     descripcion = fields.Char(string="Descripcion")
-    cuentas_cobrar_id = fields.One2many('administracion.cuentas_cobrar', 'partidas_id' ,string='Cuenta por cobrar')
+
+    cuentas_cobrar_ids = fields.One2many('administracion.cuentas_cobrar', 'partidas_id' ,string='Cuenta por cobrar')
