84e485db2d131d8ddc9c4114a6732e2ab4bb3758
Francisco Palm <francisco.palm@gmail.com>
2016-01-04 21:01:12 -0430

    Borrar una línea sobrante (PEP8) en Creditos.

--- addons/administracion/models/creditos.py
+++ addons/administracion/models/creditos.py
@@ -6,13 +6,12 @@ class Creditos(models.Model):
     _name = 'administracion.creditos'
     _rec_name = 'nro_expediente'
 
-
     solicitudes_id = fields.Many2one('solicitudes.solicitudes', string='Número de expediente')
     nro_expediente = fields.Char(string='Numero de expediente',
                                             related='solicitudes_id.nro_expediente')
     cedula_solicitante = fields.Char(string='Cédula Solicitante',
                                      related='solicitudes_id.propuestas_id.cedula_solicitante')
-    #caja_id = fields.Many2one('caja.cajas', string='Numero de expediente', required=True) # !!! Obtenido del usuario?
+    # caja_id = fields.Many2one('caja.cajas', string='Numero de expediente', required=True) # !!! Obtenido del usuario?
     solicitantes_id_propuesta = fields.Many2one(string='Solicitante',
                                             related='solicitudes_id.solicitantes_id_propuesta',
                                             readonly=True)
@@ -27,7 +26,7 @@ class Creditos(models.Model):
                                             related='solicitudes_id.tasa_interes')
     tasa_mora = fields.Float(string='Tasa de mora',
                                             related='solicitudes_id.tasa_mora')
-    #monto_cuota = fields.Float(string='Monto de la cuota') #calculado
+    # monto_cuota = fields.Float(string='Monto de la cuota') #calculado
     comision_flat = fields.Float(string='Comision FLAT',
                                             related='solicitudes_id.comision_flat')
     aporte_social = fields.Float(string='Aporte social',
@@ -36,5 +35,3 @@ class Creditos(models.Model):
     # Campos de liquidacion de credito
     fecha_liquidacion = fields.Date(string='Fecha de liquidacion',
                                             related='solicitudes_id.fecha_liquidacion')
-    fecha_ultima = fields.Date(string='Fecha ultima de pago',
-                                            related='solicitudes_id.fecha_ultima')
