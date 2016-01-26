5b275bf5f28d7afd341a2f196dec4e4c3568ef1d
Jorge Moreno <morenomeccia@yahoo.com>
2016-01-26 12:09:44 -0430

    Se modifico el modelo de unidad productiva

--- addons/propuestasfomdes/models/unidades_productivas.py
+++ addons/propuestasfomdes/models/unidades_productivas.py
@@ -14,12 +14,21 @@ class UnidadesProductivas(models.Model):
     direccion = fields.Text(string='Dirección') # de res.partner>res.company
     telefono_fijo = fields.Integer(string='Teléfono fijo')
     telefono_celular = fields.Integer(string='Teléfono Celular')
-    actividad = fields.Text(string='Actividades')
-    experiencia = fields.Text(string='Experiencia')
-    area_geografica = fields.Text(string='Area Geografica')
-    tenencia = fields.Selection(string='Tenencia', selection=[('1', 'Propio'), ('2', 'Alquilado'), ('3', 'Comodato'), ('4', 'Prestado')]) # TODO revisar las opciones en el expediente
+    actividad = fields.Char(string='Actividad que realiza')
+    experiencia = fields.Text(string='Experiencia en el area')
+    # Area de funcionamiento:
+    area_geografica = fields.Text(string='Area geografica a cubrir')
+    espacio_funcionamiento = fields.Selection(string='Espacio', selection=[('1', 'Local'), ('2', 'Galpon'), ('3', 'Finca'), ('4', 'Terreno'), ('5', 'Casa de habitacion'),('6', 'Posada'), ('7', 'Hotel'), ('8', 'Otro')])
+    situacion_propiedad = fields.Selection(string='Situacion', selection=[('1', 'Propio'), ('2', 'Alquilado'), ('3', 'Comodato'), ('4', 'Familiar'), ('5', 'Otro')])
     area_m2 = fields.Float(string='Area en metros cuadrados')
-    zona_geografica = fields.Char(string='Zona Geografica')
-    servicios = fields.Text(string='Servicios')
+    zona_geografica = fields.Char(string='Zona Geografica', selection=[('1', 'Rural'), ('2', 'Urbana')])
+    servicio_electricidad = fields.Boolean(string='Electricidad')
+    servicio_agua = fields.Boolean(string='Agua')
+    servicio_telefono = fields.Boolean(string='Telefono')
+    servicio_internet = fields.Boolean(string='Internet')
+    servicio_cabletv = fields.Boolean(string='TV por cable')
+    servicio_transporte = fields.Boolean(string='Transporte')
+    servicio_aseo = fields.Boolean(string='Aseo domiciliario')
+    servicio_vialidad = fields.Boolean(string='Vialidad')
 
     actividades_productivas_ids = fields.One2many('propuestasfomdes.actividades_productivas', 'unidades_productivas_id', string="Actividades Productivas")
