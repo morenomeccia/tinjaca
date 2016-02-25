1f5de10f4f07fded9f9a6d096990ea1d1630d45b
Jorge Moreno <morenomeccia@yahoo.com>
2016-02-25 17:07:04 -0430

    Se incluye el campo de empresa_establecida heredado desde Unidades Productivas

--- addons/propuestas/models/propuestas.py
+++ addons/propuestas/models/propuestas.py
@@ -8,10 +8,14 @@ class Propuestas(models.Model):
     _rec_name = 'codigo_planilla'
 
     solicitantes_id = fields.Many2one('propuestas.solicitantes', string="Solicitante", required=True)
-    unidades_productivas_id = fields.Many2one('propuestas.unidades_productivas', string="Unidad Productiva")
 
     codigo_planilla = fields.Char(string="Codigo Planilla", required=True)
     fecha_recibida = fields.Date(string='Fecha de recepcion', default=fields.Date.today(), required=True)
+
+    unidades_productivas_id = fields.Many2one('propuestas.unidades_productivas', string="Unidad Productiva")
+    tipos_garantia_id = fields.Many2one('politicas.tipos_garantia', string="Tipo de Garantia")
+    codigo_garantia = fields.Char(related='tipos_garantia_id.codigo', readonly=True)
+    up_empresa_establecida = fields.Boolean(related='unidades_productivas_id.empresa_establecida', readonly=True)
 
     producto_derivado = fields.Text(string='Productos derivados de la actividad')
     materia_prima = fields.Text(string='Materia prima a utilizar')
@@ -21,21 +25,17 @@ class Propuestas(models.Model):
     trabajadores = fields.Integer(string='Número de trabajadores existentes')
     puestos_trabajo = fields.Integer(string='Puestos de trabajos que se van a generar')
     explicacion = fields.Text(string='Breve explicación de la propuesta emprendedora')
-
-    tipos_garantia_id = fields.Many2one('politicas.tipos_garantia', string="Tipo de Garantia")
-    codigo_garantia = fields.Char(related='tipos_garantia_id.codigo')
+    observaciones = fields.Text(string='Observaciones')
 
     referencias_solicitante_ids = fields.One2many('propuestas.referencias_solicitante', 'propuestas_id', string="Referencias del Solicitante")
     garantias_ids = fields.One2many('propuestas.garantias', 'propuestas_id', string="Garantías")
     avalistas_ids = fields.One2many('propuestas.avalistas', 'propuestas_id', string="Avalista")
     referencias_avalista_ids = fields.One2many('propuestas.referencias_avalistas', 'propuestas_id', string='Referencias del Avalista')
     inversiones_ids = fields.One2many('propuestas.inversiones', 'propuestas_id', string='Plan de Inversiones')
+    talleres_ids = fields.Many2many('propuestas.talleres', string="Taller", relation='talleres_propuestas')
 
     inversion_total_propia = fields.Float(string='Inversión total propia') #TODO calculado!!!
     inversion_total_fomdes = fields.Float(string='Inversión total fomdes') #TODO calculado!!!
-    observaciones = fields.Text(string='Observaciones')
-
-    talleres_ids = fields.Many2many('propuestas.talleres', string="Taller", relation='talleres_propuestas')
 
     state = fields.Selection(string='Estacion', selection=[('estacion_1_propuesta_recibida', 'Propuesta recibida'),
                                                            ('estacion_2_taller', 'Taller'),
