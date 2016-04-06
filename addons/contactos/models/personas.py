2974d9d9258fc0b606e209f7dc61a99f5dedd9b6
Jorge Moreno <morenomeccia@yahoo.com>
2016-04-06 02:16:39 -0430

    Se agregan campos al modelo de Personas heredados de Partner

--- addons/contactos/models/personas.py
+++ addons/contactos/models/personas.py
@@ -7,7 +7,16 @@ class Personas(models.Model):
 
     _inherits = {'res.partner': 'partner_id'}
 
-    # overridden inherited fields to bypass access rights, in case you have
-    # access to the user but not its corresponding partner
-    #name = fields.Char(related='partner_id.name', inherited=True)
-    #email = fields.Char(related='partner_id.email', inherited=True)
+    # Campos relacionados a res.partner:
+    name = fields.Char(related='partner_id.name', string="Nombre", inherited=True)
+    foto = fields.Binary(related='partner_id.image', string="Foto")
+    pais_id = fields.Many2one(related='partner_id.country_id', string="País")
+    estado_id = fields.Many2one(related='partner_id.state_id', string="Estado")
+    ciudad = fields.Char(related='partner_id.city', string="Ciudad")
+    direccion = fields.Char(related='partner_id.street', string="Dirección")
+    direccion2 = fields.Char(related='partner_id.street2', string="Dirección 2")
+    codigo_postal = fields.Char(related='partner_id.zip', string="Codigo Postal")
+    telefono_fijo = fields.Char(related='partner_id.phone', string="Teléfono Fijo")
+    telefono_celular = fields.Char(related='partner_id.mobile', string="Teléfono Celular")
+    telefono_fax = fields.Char(related='partner_id.fax', string="Fax")
+    correo_electronico = fields.Char(related='partner_id.email', string="Correo Electrónico")
