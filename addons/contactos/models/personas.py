9516f4fc50d2264f03a122babbbc79bf018eea37
Jorge Moreno <morenomeccia@yahoo.com>
2016-04-09 22:53:41 -0430

    Se agregan nuevos campos de identificacion a Personas. Campo name calculado a partir de estos

--- addons/contactos/models/personas.py
+++ addons/contactos/models/personas.py
@@ -9,6 +9,10 @@ class Personas(models.Model):
 
     partner_id = fields.Many2one('res.partner', required=True, ondelete='restrict', string='Contacto relacionado')
 
+    cedula = fields.Char(string="Cedula", required=True)
+    apellidos = fields.Char(string="Apellidos", required=True)
+    nombres = fields.Char(string="Nombres", required=True)
+
     # Campos relacionados a res.partner:
     name = fields.Char(related='partner_id.name', string="Nombre", inherited=True)
     foto = fields.Binary(related='partner_id.image', string="Foto")
@@ -24,3 +28,13 @@ class Personas(models.Model):
     telefono_celular = fields.Char(related='partner_id.mobile', string="Teléfono Celular")
     telefono_fax = fields.Char(related='partner_id.fax', string="Fax")
     correo_electronico = fields.Char(related='partner_id.email', string="Correo Electrónico")
+
+    @api.depends('cedula', 'apellidos', 'nombres')
+    def _get_name(self):
+        """Obtiene campo name a partir de los nombres y apellidos"""
+        self.name = str(self.cedula) + " - " + str(self.nombres) + " " + str(self.apellidos)
+
+    @api.onchange('cedula', 'apellidos', 'nombres')
+    def _update_name(self):
+        self._get_name()
+
